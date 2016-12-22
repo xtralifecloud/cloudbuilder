54c372f1d5d9f9ef7bc13c01fdcc42948473bd30
Michael El Baki <michael@xtralife.cloud>
2016-12-22 11:55:50 +0100

    Modified some methods to have the handler parameter at the end of the list of parameters.

--- CloudBuilder/sources/HighLevel/CTribeManager.cpp
+++ CloudBuilder/sources/HighLevel/CTribeManager.cpp
@@ -37,79 +37,99 @@ namespace CloudBuilder {
 		managerSingleton.Release();
 	}
 
-	void CTribeManager::ListUsers(const char *containsString, int limit, int skip, CResultHandler *handler)
-	{
-		if (!CClan::Instance()->isUserLogged()) { InvokeHandler(handler, enNotLogged); return; }
-		CHJSON json;
-		json.Put("q",containsString);
-		json.Put("skip",skip);
-		json.Put("limit",limit);
-		return CClannishRESTProxy::Instance()->ListUsers(&json, MakeBridgeDelegate(handler));
-	}
-	
-	void CTribeManager::ListFriends(CResultHandler *handler, const char *aDomain) {
-		if (!CClan::Instance()->isUserLogged()) { InvokeHandler(handler, enNotLogged); return; }
-		CHJSON json;
-		json.Put("domain", aDomain);
-		return CClannishRESTProxy::Instance()->ListFriends(&json, MakeBridgeDelegate(handler));
+    void CTribeManager::ListUsers(const char *aContainsString, int aLimit, int aSkip, CResultHandler *aHandler)
+    {
+        if (!CClan::Instance()->isUserLogged()) { InvokeHandler(aHandler, enNotLogged); return; }
+        CHJSON json;
+        json.Put("q",aContainsString);
+        json.Put("skip",aSkip);
+        json.Put("limit",aLimit);
+        CClannishRESTProxy::Instance()->ListUsers(&json, MakeBridgeDelegate(aHandler));
+    }
+    
+    void CTribeManager::ListUsers(CResultHandler *aHandler, const char *aContainsString, int aLimit, int aSkip)
+    {
+        this->ListUsers(aContainsString, aLimit, aSkip, aHandler);
+    }
+    
+    void CTribeManager::ListFriends(const char *aDomain, CResultHandler *aHandler) {
+        if (!CClan::Instance()->isUserLogged()) { InvokeHandler(aHandler, enNotLogged); return; }
+        CHJSON json;
+        json.Put("domain", aDomain);
+        CClannishRESTProxy::Instance()->ListFriends(&json, MakeBridgeDelegate(aHandler));
+    }
+    
+	void CTribeManager::ListFriends(CResultHandler *aHandler, const char *aDomain) {
+        this->ListFriends(aDomain, aHandler);
 	}
 			
-	void CTribeManager::BlacklistFriends(CResultHandler *handler, const char *aDomain) {
-		if (!CClan::Instance()->isUserLogged()) { InvokeHandler(handler, enNotLogged); return; }
-		CHJSON json;
-		json.Put("domain", aDomain);
-		return CClannishRESTProxy::Instance()->BlacklistFriends(&json, MakeBridgeDelegate(handler));
-	}
-	
-	void CTribeManager::ChangeRelationshipStatus(CResultHandler *handler, const CotCHelpers::CHJSON *aOptions) {
-		if (!CClan::Instance()->isUserLogged()) { InvokeHandler(handler, enNotLogged); return; }
-		if (aOptions == NULL) { InvokeHandler(handler, enBadParameters); return; }
-		if (IsEqual(aOptions->GetString("id", ""), CClannishRESTProxy::Instance()->GetGamerID())) { InvokeHandler(handler, enFriendYourself); return; }
-		return CClannishRESTProxy::Instance()->ChangeRelationshipStatus(aOptions, MakeBridgeDelegate(handler));
-		
-	}
-	
-	void CTribeManager::ListNetworkFriends(CResultHandler *handler, const CotCHelpers::CHJSON* aConfiguration)
-	{
-		const char *network = aConfiguration->GetString("network");
-		
-		CONSOLE_VERBOSE("List network friends\n");
-		if (!CClan::Instance()->isUserLogged()) { InvokeHandler(handler, enNotLogged); return; }
-		
-		if (IsEqual(network, "googleplus")) {
-			return InvokeHandler(handler, enExternalCommunityNotSetup, "Obsolete network");
-		} else if (IsEqual(network, "facebook")) {
-			return InvokeHandler(handler, enExternalCommunityNotSetup, "Obsolete network");
-		} else if (IsEqual(network, "googleplusId")) {
-			return CClannishRESTProxy::Instance()->GetNetworkFriends("googleplus", aConfiguration, MakeBridgeDelegate(handler));
-		} else if (IsEqual(network, "facebookId")) {
-			return CClannishRESTProxy::Instance()->GetNetworkFriends("facebook", aConfiguration, MakeBridgeDelegate(handler));
-		} else if (IsEqual(network, "gamecenter")) {
-			// List game center friends -- will continue to ListFriendsGCDone
-			struct ListedFriendsGC: CInternalResultHandler {
-				_BLOCK3(ListedFriendsGC, CInternalResultHandler,
-						CTribeManager *, self,
-						CHJSON *, mConfig,
-						CResultHandler *, resultHandler);
-				void Done(const CCloudResult *result) {
-					owned_ref<CHJSON> config (mConfig); // To ensure release when exited
-
-					self->ListFriendsGCDone(result, config, resultHandler);
-				}
-			};
-			GameCenter::listFriends(new ListedFriendsGC(this, aConfiguration->Duplicate(), handler));
-			
-		} else {
-			return InvokeHandler(handler, enExternalCommunityNotSetup, "Unrecognized network");
-		}
-		
-	}
-	
-	void CTribeManager::ListNetworkFriends(CResultHandler *handler, const char *network, const CotCHelpers::CHJSON* aFriends ) {
+    void CTribeManager::BlacklistFriends(const char *aDomain, CResultHandler *aHandler) {
+        if (!CClan::Instance()->isUserLogged()) { InvokeHandler(aHandler, enNotLogged); return; }
+        CHJSON json;
+        json.Put("domain", aDomain);
+        CClannishRESTProxy::Instance()->BlacklistFriends(&json, MakeBridgeDelegate(aHandler));
+    }
+    
+    void CTribeManager::BlacklistFriends(CResultHandler *aHandler, const char *aDomain) {
+        this->BlacklistFriends(aDomain, aHandler);
+    }
+    
+    void CTribeManager::ChangeRelationshipStatus(const CotCHelpers::CHJSON *aOptions, CResultHandler *aHandler) {
+        if (!CClan::Instance()->isUserLogged()) { InvokeHandler(aHandler, enNotLogged); return; }
+        if (aOptions == NULL) { InvokeHandler(aHandler, enBadParameters); return; }
+        if (IsEqual(aOptions->GetString("id", ""), CClannishRESTProxy::Instance()->GetGamerID())) { InvokeHandler(aHandler, enFriendYourself); return; }
+        CClannishRESTProxy::Instance()->ChangeRelationshipStatus(aOptions, MakeBridgeDelegate(aHandler));
+    }
+    
+    void CTribeManager::ChangeRelationshipStatus(CResultHandler *aHandler, const CotCHelpers::CHJSON *aOptions) {
+        this->ChangeRelationshipStatus(aOptions, aHandler);
+    }
+    
+    void CTribeManager::ListNetworkFriends(const CotCHelpers::CHJSON* aConfiguration, CResultHandler *aHandler)
+    {
+        const char *network = aConfiguration->GetString("network");
+        
+        CONSOLE_VERBOSE("List network friends\n");
+        if (!CClan::Instance()->isUserLogged()) { InvokeHandler(aHandler, enNotLogged); return; }
+        
+        if (IsEqual(network, "googleplus")) {
+            InvokeHandler(aHandler, enExternalCommunityNotSetup, "Obsolete network");
+        } else if (IsEqual(network, "facebook")) {
+            InvokeHandler(aHandler, enExternalCommunityNotSetup, "Obsolete network");
+        } else if (IsEqual(network, "googleplusId")) {
+            CClannishRESTProxy::Instance()->GetNetworkFriends("googleplus", aConfiguration, MakeBridgeDelegate(aHandler));
+        } else if (IsEqual(network, "facebookId")) {
+            CClannishRESTProxy::Instance()->GetNetworkFriends("facebook", aConfiguration, MakeBridgeDelegate(aHandler));
+        } else if (IsEqual(network, "gamecenter")) {
+            // List game center friends -- will continue to ListFriendsGCDone
+            struct ListedFriendsGC: CInternalResultHandler {
+                _BLOCK3(ListedFriendsGC, CInternalResultHandler,
+                        CTribeManager *, self,
+                        CHJSON *, mConfig,
+                        CResultHandler *, resultHandler);
+                void Done(const CCloudResult *result) {
+                    owned_ref<CHJSON> config (mConfig); // To ensure release when exited
+                    
+                    self->ListFriendsGCDone(result, config, resultHandler);
+                }
+            };
+            GameCenter::listFriends(new ListedFriendsGC(this, aConfiguration->Duplicate(), aHandler));
+            
+        } else {
+            InvokeHandler(aHandler, enExternalCommunityNotSetup, "Unrecognized network");
+        }
+    }
+    
+    void CTribeManager::ListNetworkFriends(CResultHandler *aHandler, const CotCHelpers::CHJSON* aConfiguration)
+    {
+        this->ListNetworkFriends(aConfiguration, aHandler);
+    }
+    
+	void CTribeManager::ListNetworkFriends(CResultHandler *aHandler, const char *network, const CotCHelpers::CHJSON* aFriends ) {
 		CHJSON config;
 		config.Put("network" , network);
 		config.Put("friends" , aFriends->Duplicate());
-		ListNetworkFriends(handler, &config);
+		ListNetworkFriends(aHandler, &config);
 	}
 	
 	void CTribeManager::ListFriendsGCDone(const CCloudResult *result, CotCHelpers::CHJSON* aConfiguration, CResultHandler *callNext) {
@@ -135,18 +155,27 @@ namespace CloudBuilder {
 		delete j;
 	}
 
-	void CTribeManager::FriendsBestHighScore(CResultHandler *handler, int aCount, int aPage, const char *aMode, const char *aDomain)
-	{
-		CHJSON json;
-		json.Put("count", aCount);
-		json.Put("mode", aMode);
-		json.Put("page", aPage);
-		json.Put("domain", aDomain);
-		return CClannishRESTProxy::Instance()->FriendsBestHighScore(&json, MakeBridgeDelegate(handler));
-	}
-
-	void CTribeManager::FindOpponents(CResultHandler *handler, const CotCHelpers::CHJSON *aFilter, const char *aDomain) {
-		return CClannishRESTProxy::Instance()->FindOpponents(aDomain, aFilter, MakeBridgeDelegate(handler));
-	}
+    void CTribeManager::FriendsBestHighScore(int aCount, int aPage, const char *aMode, const char *aDomain, CResultHandler *aHandler)
+    {
+        CHJSON json;
+        json.Put("count", aCount);
+        json.Put("mode", aMode);
+        json.Put("page", aPage);
+        json.Put("domain", aDomain);
+        CClannishRESTProxy::Instance()->FriendsBestHighScore(&json, MakeBridgeDelegate(aHandler));
+    }
+    
+    void CTribeManager::FriendsBestHighScore(CResultHandler *aHandler, int aCount, int aPage, const char *aMode, const char *aDomain)
+    {
+        this->FriendsBestHighScore(aCount, aPage, aMode, aDomain, aHandler);
+    }
+    
+    void CTribeManager::FindOpponents(const CotCHelpers::CHJSON *aFilter, const char *aDomain, CResultHandler *aHandler) {
+        CClannishRESTProxy::Instance()->FindOpponents(aDomain, aFilter, MakeBridgeDelegate(aHandler));
+    }
+    
+    void CTribeManager::FindOpponents(CResultHandler *aHandler, const CotCHelpers::CHJSON *aFilter, const char *aDomain) {
+        this->FindOpponents(aFilter, aDomain, aHandler);
+    }
 }
 
