e5d898f08a2967cd678687c39cd5ad653e2eb8e5
Michael El Baki <michael@xtralife.cloud>
2017-10-05 11:37:02 +0200

    Upgraded to minSdkVersion 16, targetSdkVersion 25, Android build tools 26, GooglePlay Services 11.4.0 and Android Support libraries 25.3.1

--- CLI/Android/jni/Application.mk
+++ CLI/Android/jni/Application.mk
@@ -1,4 +1,4 @@
 # The ARMv7 is significanly faster due to the use of the hardware FPU
 APP_ABI         := armeabi armeabi-v7a x86 x86_64 arm64-v8a
-APP_PLATFORM	:= android-14
+APP_PLATFORM	:= android-25
 APP_STL			:= stlport_static
--- CloudBuilder/CloudBuilderAndroid/CloudBuilder/jni/Application.mk
+++ CloudBuilder/CloudBuilderAndroid/CloudBuilder/jni/Application.mk
@@ -1,5 +1,5 @@
 # The ARMv7 is significanly faster due to the use of the hardware FPU
-APP_PLATFORM := android-21
+APP_PLATFORM := android-25
 APP_ABI         := armeabi armeabi-v7a x86 x86_64 arm64-v8a
 
 APP_STL := gnustl_static
